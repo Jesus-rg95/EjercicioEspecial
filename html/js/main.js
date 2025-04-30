575957da6a4115725fb9baceb11e9cc4b05762fa
jesusgr <jesusgr9511@gmail.com>
2025-04-30 10:33:01 -0600

    Se agregan cambios

---
+++ html/js/main.js
@@ -0,0 +1,4 @@
+const main = document.getElementsByTagName("").item(0);
+let mainProds = document.getElementById("");
+const ulMenu = document.getElementById("")
+const URLMain = "https://github.com/jcgeneration/EjercicioEspecial/fork";
